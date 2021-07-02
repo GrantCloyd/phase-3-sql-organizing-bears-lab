2a90b54fae9ebb5d41cd63d48a85642127239258
Grant Cloyd <dgcloyd@gmail.com>
2021-07-02 13:21:40 -0500

    commit tree 4c489bc30c174a60f3bcdd6be013c0566921a90c

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 6, "M", "brown", "ill-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Rowdy', 6, "M", "brown", "ill-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Tabitha', 6, "M", "brown", "ill-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Sergeant Brown', 6, "M", "brown", "ill-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Melissa', 6, "M", "brown", "ill-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Grinch', 6, "M", "brown", "ill-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Wendy', 6, "M", "brown", "ill-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+(NULL, null, null, null, null, null)
